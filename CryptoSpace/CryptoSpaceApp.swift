1a7c19304e3edc4f097cde6ba1e38b84a711d907
horatiu iesanu <horas03@gmail.com>
2024-11-14 13:18:53 +0200

    Initial Commit

---
+++ CryptoSpace/CryptoSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSpaceApp.swift
+//  CryptoSpace
+//
+//  Created by Horatiu Iesanu on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
